6eac48c3508a4ee787c43de15721fec4d1525ddd
bdwat <92316421+bdwat@users.noreply.github.com>
2022-09-12 16:24:30 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,35 @@
+let mensagem = document.querySelector(".mensagem") ;
+let mensagem1 = document.querySelector('.mensagem1')
+let mensagem2 = document.querySelector('.mensagem2')
+
+
+// mostra a mensagem
+function showMessage(){   
+   mensagem.style.display = "block";   
+   console.log('a')
+ }
+// esconde a mensagem
+function hideMessage(){
+  mensagem.style.display = "none"; 
+  console.log('b')
+}
+
+function showMessage1(){   
+    mensagem1.style.display = "block";   
+    console.log('a')
+}
+
+function hideMessage1(){
+    mensagem1.style.display = "none"; 
+    console.log('b')
+  }
+
+function showMessage2(){   
+    mensagem2.style.display = "block";   
+    console.log('a')
+}
+
+function hideMessage2(){
+    mensagem2.style.display = "none"; 
+    console.log('b')
+  }
